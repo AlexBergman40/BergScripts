42ba76756efb76baf48fa445e15f7e237502bd57
Alex Bergman <alexbergman40@gmail.com>
2024-12-20 20:48:03 -0800

    Unstaggered the bits. Every 4 bits in a byte represents a pixel now.

--- export_to_brg.js
+++ export_to_brg.js
@@ -13,46 +13,44 @@ function getPaletteIndex(x, y)
     return pixelC;
 }
 
-function setPixelIndex(array, index, row, col)
+function setPixelIndex(byte, colorindex, x)
 {
-    for (var i = 0; i < 4; i++)
-    {
-        array[ row+(i*8) ] |= (((index >> i) & 1) << col);
-    }
+    //half a byte per pixel
+    //x%2 = 0 left half 
+    //x%2 = 1 right half
+    //reading left to right
+    byte |= (colorindex << (1 - (x % 2))*4)
+
+    return byte
 }
 
-//This function builds each tile as an 32 byte Uint8Array.
-//Each row of pixels is represented by 4 bytes, row n is represented by
-//bytes n, n+8, n+16, n+24
-//Each pixel in the row has a color index from 0 to 15, in binary.
-//pixel x,y is byte y+24 bit x, byte y+16 bit x, byte y+8 bit x, byte y bit x
 
+
+//build a uint8array which holds the index of two pixels per byte.
+//even pixels are stored in the left half of the byte
+//odd pixels are stored in the right half of the byte
 function buildTileArray()
 {
     var out = [];
 
-    for (var y = 0; y < image.height; y += TILE_SIZE){
-        for (var x = 0; x < image.width; x += TILE_SIZE){
+    for (var y = 0; y < image.height; y++){
+        for (var x = 0; x < image.width; x ++){
 
-            var tile = new Uint8Array(8*4);
+            var byte = Uint8Array[1];
 
-            for (var row = 0; row < TILE_SIZE; row++){
-                for (var col = 0; col < TILE_SIZE; col++){
+            //left half of the byte
+            colorIndex = getPaletteIndex(x, y);
+            byte = setPixelIndex( byte, colorIndex, x );
 
-                    colorIndex = getPaletteIndex(col + x, row + y);
-                    setPixelIndex(tile, colorIndex, row, col);
+            //right half of the byte
+            colorIndex = getPaletteIndex(++x, y);
+            byte = setPixelIndex( byte, colorIndex, x );
 
-                }
-            }
-
-            out.push(tile);
+            out.push(byte);
         }
     }
 
-    var combinedArray = new Uint8Array(out.length * 32);
-    for (var i = 0; i < out.length; i++){
-        combinedArray.set(out[i], i * 32);
-    }
+    var combinedArray = new Uint8Array(out);
 
     return combinedArray;
 }
